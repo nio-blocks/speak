c10295bcbebe1cc239a9da000be8b1eb45ee8f22
Tom Young <tyoung@n.io>
2019-02-05 23:59:06 +0000

    add Rate and Volume properties

--- tests/test_speak_block.py
+++ tests/test_speak_block.py
@@ -11,10 +11,16 @@ class TestExample(NIOBlockTestCase):
         """Messages are queued for every signal."""
         mock_engine = mock_init.return_value = Mock()
         blk = Speak()
-        self.configure_block(blk, {"message":"{{$bar}}"})
+        self.configure_block(blk, {
+            "message": "{{ $bar }}",
+            "volume": 0.5,
+            "rate": 42,
+        })
         blk.start()
         mock_init.assert_called_once_with()
-        mock_engine.setProperty.assert_called_once_with("volume", 1)
+        self.assertEqual(mock_engine.setProperty.call_count, 2)
+        self.assertEqual(mock_engine.setProperty.call_args_list[0][0], ("rate", 42))
+        self.assertEqual(mock_engine.setProperty.call_args_list[1][0], ("volume", 0.5))
         blk.process_signals([Signal({"bar": "nio foo foo"})])
         mock_engine.say.assert_called_once_with("nio foo foo")
         mock_engine.runAndWait.assert_called_once_with()
